afcf7f344a429b7ac48d77021ef6fde59081bba5
микро заврик <117584790+microzavrik@users.noreply.github.com>
2024-05-18 17:13:00 +0300

    Add files via upload

---
+++ local_cloud/client/cloud_client.hpp
@@ -0,0 +1,24 @@
+#ifndef CLOUD_CLIENT_HPP
+#define CLOUD_CLIENT_HPP
+
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+
+namespace network
+{
+    class cloud_client
+    {
+    public:
+        cloud_client(boost::asio::io_context& ios, const std::string& server_ip, 
+            const std::string& server_port);
+        void start_listening();
+        void send_message(const std::string& message);
+    private:
+        boost::asio::ip::tcp::socket socket_;
+        enum { max_length = 1024 };
+        char data_[max_length];
+    };
+}
+
+#endif
